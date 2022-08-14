c9327847ff99f924d671367da51c033e25727533
Daniel <daniel-mcintyre@hotmail.com>
2022-08-14 17:10:39 -0400

    updates to server

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
