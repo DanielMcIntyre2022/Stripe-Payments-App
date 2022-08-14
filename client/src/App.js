6b27d3273e3d5a446e729e381eca059869f89fe9
Daniel <daniel-mcintyre@hotmail.com>
2022-08-14 17:21:08 -0400

    updates to front-end checkout button logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
 
-const buttonClick = (e) => {
-  console.log('Checkout')
+const buttonClick = () => {
+  fetch('/create-checkout-session', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      items: [
+        { id: 1, quanity: 3 },
+        { id: 2, quanity: 1 }
+      ]
+    })
+  }).then(res => {
+    if (res.ok) return res.json()
+    return res.json().then(json => Promise.reject(json))
+  }).then(({ url }) => {
+    window.location = url
+  }).catch(e => {
+    console.log(e);
+  })
 }
 
 function App() {
