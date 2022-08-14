c4deedc16e3db823866ffd38b57509ff824375d2
Daniel <daniel-mcintyre@hotmail.com>
2022-08-14 17:00:27 -0400

    server setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
+
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+const storeItems = new Map([
+    [1, { priceInCents: 10000, name: 'Testing Stripe Payments is easy!' }],
+    [2, { priceInCents: 20000, name: 'Raptors Championship run!'}]
+])
 
 const PORT = process.env.PORT || 6000;
 
